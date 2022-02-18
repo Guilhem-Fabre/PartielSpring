eab601a4ef670ca0f84aada00fb49f51709d5a4f
GuilhemFabre <guilhem.fabre@cegetel.net>
2022-02-18 16:30:31 +0100

    Partie 3

--- WeatherPartiel/src/test/java/com/example/WeatherPartiel/WeatherPartielApplicationTests.java
+++ WeatherPartiel/src/test/java/com/example/WeatherPartiel/WeatherPartielApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.WeatherPartiel;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherPartielApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
